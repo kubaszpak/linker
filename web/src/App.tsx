2d20bd25b571c9b6f63df4dd7c796aa6442c6326
szpak <jakub.szpak@nokia.com>
2021-08-31 19:16:24 +0200

    Example Navbar added

--- web/src/App.tsx
+++ web/src/App.tsx
@@ -1,19 +1,21 @@
 import './App.css'
 import { Typography, Button } from '@material-ui/core'
-import LinkOutlinedIcon from '@material-ui/icons/LinkOutlined'
+import LinkIcon from '@material-ui/icons/Link'
+import Navbar from './components/Navbar'
 
 function App() {
   return (
     <div className="App">
+      <Navbar />
       <header className="App-header">
-        <Typography variant="h1">Welcome</Typography>
+        <Typography variant="h2">Welcome</Typography>
       </header>
       <Button variant="contained" color="primary" style={{
         position: "absolute",
         bottom: 50,
         right: 50
       }}>
-        <LinkOutlinedIcon fontSize="large" />
+        <LinkIcon fontSize="large" />
       </Button>
     </div>
   );
---
+++ web/src/components/Navbar.tsx
@@ -0,0 +1,156 @@
+import React from 'react'
+import { alpha, withStyles } from '@material-ui/core/styles'
+import { AppBar, Typography, IconButton, Toolbar, Badge, InputBase } from '@material-ui/core'
+// import { MenuIcon, SearchIcon, NotificationsIcon, AccountCircle, MailIcon } from '@material-ui/icons'
+import MenuIcon from '@material-ui/icons/Menu'
+import SearchIcon from '@material-ui/icons/Search'
+import NotificationsIcon from '@material-ui/icons/Notifications'
+import MailIcon from '@material-ui/icons/Mail'
+import AccountCircle from '@material-ui/icons/AccountCircle'
+import MoreIcon from '@material-ui/icons/More'
+
+// @ts-ignore
+const styles = theme => ({
+    grow: {
+        flexGrow: 1
+    },
+    menuButton: {
+        marginRight: theme.spacing(2)
+    },
+    title: {
+        display: "none",
+        [theme.breakpoints.up("sm")]: {
+            display: "block"
+        }
+    },
+    search: {
+        position: "relative",
+        borderRadius: theme.shape.borderRadius,
+        backgroundColor: alpha(theme.palette.common.white, 0.15),
+        "&:hover": {
+            backgroundColor: alpha(theme.palette.common.white, 0.25)
+        },
+        marginRight: theme.spacing(2),
+        marginLeft: 0,
+        width: "100%",
+        [theme.breakpoints.up("sm")]: {
+            marginLeft: theme.spacing(3),
+            width: "auto"
+        }
+    },
+    searchIcon: {
+        width: theme.spacing(7),
+        height: "100%",
+        position: "absolute",
+        pointerEvents: "none",
+        display: "flex",
+        alignItems: "center",
+        justifyContent: "center"
+    },
+    inputRoot: {
+        color: "inherit"
+    },
+    inputInput: {
+        padding: theme.spacing(1, 1, 1, 7),
+        transition: theme.transitions.create("width"),
+        width: "100%",
+        [theme.breakpoints.up("md")]: {
+            width: 200
+        }
+    },
+    sectionDesktop: {
+        display: "none",
+        [theme.breakpoints.up("md")]: {
+            display: "flex"
+        }
+    },
+    sectionMobile: {
+        display: "flex",
+        [theme.breakpoints.up("md")]: {
+            display: "none"
+        }
+    }
+});
+
+
+class Navbar extends React.Component {
+
+    render() {
+        // @ts-ignore
+        const { classes } = this.props;
+        const menuId = "primary-search-account-menu";
+        const mobileMenuId = "primary-search-account-menu-mobile";
+
+        return (
+            <div className={classes.grow} >
+                <AppBar position="static">
+                    <Toolbar>
+                        <IconButton
+                            edge="start"
+                            className={classes.menuButton}
+                            color="inherit"
+                            aria-label="open drawer"
+                        >
+                            <MenuIcon />
+                        </IconButton>
+                        <Typography className={classes.title} variant="h6" noWrap>
+                            Material-UI
+                        </Typography>
+                        <div className={classes.search}>
+                            <div className={classes.searchIcon}>
+                                <SearchIcon />
+                            </div>
+                            <InputBase
+                                placeholder="Search…"
+                                classes={{
+                                    root: classes.inputRoot,
+                                    input: classes.inputInput
+                                }}
+                                inputProps={{ "aria-label": "search" }}
+                            />
+                        </div>
+                        <div className={classes.grow} />
+                        <div className={classes.sectionDesktop}>
+                            <IconButton aria-label="show 4 new mails" color="inherit">
+                                <Badge badgeContent={4} color="secondary">
+                                    <MailIcon />
+                                </Badge>
+                            </IconButton>
+                            <IconButton
+                                aria-label="show 17 new notifications"
+                                color="inherit"
+                            >
+                                <Badge badgeContent={17} color="secondary">
+                                    <NotificationsIcon />
+                                </Badge>
+                            </IconButton>
+                            <IconButton
+                                edge="end"
+                                aria-label="account of current user"
+                                aria-controls={menuId}
+                                aria-haspopup="true"
+                                color="inherit"
+                            >
+                                <AccountCircle />
+                            </IconButton>
+                        </div>
+                        <div className={classes.sectionMobile}>
+                            <IconButton
+                                aria-label="show more"
+                                aria-controls={mobileMenuId}
+                                aria-haspopup="true"
+                                color="inherit"
+                            >
+                                <MoreIcon />
+                            </IconButton>
+                        </div>
+                    </Toolbar>
+                </AppBar>
+            </div>
+        )
+
+    }
+}
+
+// @ts-ignore
+export default withStyles(styles)(Navbar)
